6b425ce8128cc3467ed5dab1a9090357a5566144
Anurag <anuragraveendren98@gmail.com>
2024-01-24 20:46:00 +0530

    productdetailsAddsize

--- controllers/user/subcategory.js
+++ controllers/user/subcategory.js
@@ -2,17 +2,18 @@ const subcategory=require('../../models/admin/subcategory')
 
 let getsubcategories = async (req, res) => {
     try {
-        console.log("Before find");
+        // console.log("Before find");
         const subcategor = await subcategory.find({}, 'id title description image');
         console.log("After find");
 
-        console.log(subcategor); // Add this line to see what subcategor is
+        // console.log(subcategor); // Add this line to see what subcategor is
         res.render('user/categories', { subcategor });
     } catch (error) {
         console.error(error);
         res.status(500).json({ success: false });
     }
 };
+
 
 
 module.exports={getsubcategories}
