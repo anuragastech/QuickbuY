4214ae0261dd179d7f76599ecf5c936897334695
Anurag <anuragraveendren98@gmail.com>
2024-03-12 20:50:29 +0530

    new name for project updated

--- controllers/user/home.js
+++ controllers/user/home.js
@@ -22,7 +22,7 @@ const getproductDataIn = async (req, res) => {
             const prdcts = await product.find().populate('category').populate('subcategory');
             const homebanner = await Homepagepic.find({}, 'h1 h2 image');
             const Homepagepics = await HomepageFooter.find();
-            const prdctSliced = prdcts.slice(0, 9); // Limit to 8 products
+            const prdctSliced = prdcts.slice(0, 18); // Limit to 8 products
 
             // Render the home page with product data
             res.render('user/index', { subcategor, prdct: prdctSliced, categorys, homebanner, Homepagepics, loggedIn: true });
