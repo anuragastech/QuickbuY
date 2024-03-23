bb190ee3588186f038be28d67b829188cb386c42
Anurag <anuragraveendren98@gmail.com>
2024-03-23 23:31:28 +0530

    updated

--- config/chart.js
+++
@@ -1,9 +0,0 @@
-// const { CanvasRenderService } = require('chartjs-node-canvas'); // Include chartjs-node-canvas
-// const chartConfig = require('./chartConfig'); // Import chart configuration
-
-
-// // Function to render the chart
-// async function renderChart() {
-//     const canvasRenderService = new CanvasRenderService(800, 600);
-//     return await canvasRenderService.renderToBuffer(chartConfig);
-// }
